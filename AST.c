c262f234d5b5656ea47125ada7231e22ec85f587
Alexis Pichon <alexis.pichon@etu.u-bordeaux.fr>
2017-03-15 13:27:21 +0100

    modifié :         AST.c 	modifié :         iimp.l

--- AST.c
+++ AST.c
@@ -106,9 +106,82 @@ static int eval_node(ENV* e, NODE nd)
     }
 }
 
+static char *opstr(int op)
+{
+    switch (op)
+    {
+        case (Se) :
+            return ("Se");
+        case (Wh) :
+            return ("Wh");
+        case (If) :
+            return ("IfThEl");
+        case (Sk) :
+            return ("skip");
+        case (Af) :
+            return ("Af");
+        case (Pl) :
+            return ("+");
+        case (Mo) :
+            return ("-");
+        case (Mu) :
+            return ("*");
+        default :
+            return NULL;
+    }
+}
+
+static inline void ecrire_CST(CST cst)
+{ fprintf(stdout, "%d", cst.value); }
+
+static inline void ecrire_ID(ID id)
+{ fprintf(stdout, "%s", id.value); }
+
+static int ecrire_OP(OP opr)
+{
+    fprintf(stdout, " %s", opstr(opr.op));
+    for (int i = 0; i < opr.nargs; ++i)
+    {
+        if (opr.operands[i])
+        {
+            switch (opr.operands[i]->type)
+            {
+                case (cstTYPE) :
+                {
+                    fprintf(stdout, " ");
+                    ecrire_CST(opr.operands[i]->cst);
+                    break;
+                }
+                case (idTYPE) :
+                {
+                    fprintf(stdout, " ");
+                    ecrire_ID(opr.operands[i]->id);
+                    break;
+                }
+                case (opTYPE) :
+                {
+                    ecrire_OP(opr.operands[i]->opr);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+int ecrire_AST(NODE nd)
+{
+    if (!nd)
+        return 0;
+    fprintf(stdout, "Mp");
+    ecrire_OP(nd->opr);
+    fprintf(stdout, "\n");
+    return 0;
+}
+
 int evalexpr(ENV* environ, NODE nd)
 {
     int res = eval_node(environ, nd);
     ecrire_env(*environ);
+    ecrire_AST(nd);
     return res;
 }
